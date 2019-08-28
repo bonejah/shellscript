5666cd4530ccfd483661f0ea94839abc2fe7bce0
Bruno Lima <brunopclima81@gmail.com>
2019-08-28 06:36:46 -0300

    add files

---
+++ scripts/script2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This program is to used to training shell script
+# Use script1.sh or script2.sh
+#
+
+
+NOME="Bianca"
+echo "Hello ${NOME}"
+NOME="vicek"
+echo "Hello {NOME}"
+echo "Enter your name: "
+read MEU_NOME
+echo "ola ${MEU_NOME}"
+
+
+
+
+
+
---
+++ scripts/script3.sh
@@ -0,0 +1,9 @@
+#!/bin.bash
+#
+# This programa is abou subshell and changing commands
+
+cd /tmp
+touch file1.txt
+ls -l /tmp/file1.txt\
+pwd
+
