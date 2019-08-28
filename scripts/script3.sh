914dd5d451282a2a29b167612a989a15c940679c
Bruno Lima <brunopclima81@gmail.com>
2019-08-28 07:01:33 -0300

    add more exercises

--- scripts/script3.sh
+++ scripts/script3.sh
@@ -1,9 +1,18 @@
-#!/bin.bash
+#!/bin/bash
 #
-# This programa is abou subshell and changing commands
+# This programa is about subshell and changing commands
 
 cd /tmp
-touch file1.txt
-ls -l /tmp/file1.txt\
+touch file1
+ls -l /tmp/file1
 pwd
+
+# Assign a date to a variable date
+DATA=$(date +%d-%m-%y)
+echo ${DATA}
+
+# Create a file with the date
+cd ~/Developer/Git/shellscript/scripts
+touch file-${DATA}.txt
+
 
---
+++ scripts/script4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# A simple program using args
+echo "Arg $1"
+echo "Arg $2"
+
+# Numbers args assign
+echo "Sum args assigned: $#"
+
+# List all args
+echo "List all args assigned: $@"
+
+
